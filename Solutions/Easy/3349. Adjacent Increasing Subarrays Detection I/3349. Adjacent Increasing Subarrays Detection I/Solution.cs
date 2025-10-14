13f4401a8927532b81cce8f06fb602d4b987ddf9
Matt-Ranasinghe <matt.mailathome@gmail.com>
2025-10-14 23:53:01 +0100

    Daily Question CS only for now 14/10/25

---
+++ Solutions/Easy/3349. Adjacent Increasing Subarrays Detection I/3349. Adjacent Increasing Subarrays Detection I/Solution.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Solution {
+    public bool HasIncreasingSubarrays(IList<int> nums, int k) {
+        int n = nums.Count;
+        if(n < 2 * k) return false;
+        bool[] increasing = new bool[n];
+        int length = 0, prev = -1001;
+        for(int i = 0; i < n; i++){
+            if(prev < nums[i]){
+                length++;
+            }
+            else {
+                length = 1;
+            }
+            if(length >= k){
+                increasing[i] = true;
+                if(i >= k && increasing[i - k]) return true;
+            }
+            prev = nums[i];
+        }
+        return false;
+    }
+}
--- Solutions/Normal/516. Longist Palindromic Subsequence/Solution.cs
+++ Solutions/Normal/516. Longist Palindromic Subsequence/Solution.cs
@@ -2,34 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace _516._Longist_Palindromic_Subsequence
+class Solution
 {
-    class Solution
+    public int LongestPalindromeSubseq(string s)
     {
-        public int LongestPalindromeSubseq(string s) {
-            int n = s.Length;
-            int[,] dp = new int[n,n];
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    dp[i,j] = -1;
-                }
+        int n = s.Length;
+        int[,] dp = new int[n, n];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                dp[i, j] = -1;
             }
-            return LongestSequence(0, n - 1, s, dp);
         }
-        private int LongestSequence(int left, int right, string s, int[,] dp){
-            if(left > right) return 0;
-            if(left == right) return 1;
-            if(dp[left,right] != -1){
-                return dp[left, right];
-            }
-            else if(s[left] == s[right]){
-                dp[left,right] = LongestSequence(left + 1, right - 1, s, dp) + 2;
-                return dp[left, right];
-            }
-            else{
-                dp[left, right] = Math.Max(LongestSequence(left, right - 1, s, dp), LongestSequence(left + 1, right, s, dp));
-                return dp[left, right];
-            }
+        return LongestSequence(0, n - 1, s, dp);
+    }
+    private int LongestSequence(int left, int right, string s, int[,] dp)
+    {
+        if (left > right) return 0;
+        if (left == right) return 1;
+        if (dp[left, right] != -1)
+        {
+            return dp[left, right];
+        }
+        else if (s[left] == s[right])
+        {
+            dp[left, right] = LongestSequence(left + 1, right - 1, s, dp) + 2;
+            return dp[left, right];
+        }
+        else
+        {
+            dp[left, right] = Math.Max(LongestSequence(left, right - 1, s, dp), LongestSequence(left + 1, right, s, dp));
+            return dp[left, right];
         }
     }
 }
