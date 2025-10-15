5491c6bb2223998456690299e74da19f3c046df4
Matt-Ranasinghe <matt.mailathome@gmail.com>
2025-10-15 16:41:17 +0100

    Renamed a variable.

--- Solutions/Normal/3350. Adjacent Increasing Subarrays Detection II/3350. Adjacent Increasing Subarrays Detection II/Solution.cs
+++ Solutions/Normal/3350. Adjacent Increasing Subarrays Detection II/3350. Adjacent Increasing Subarrays Detection II/Solution.cs
@@ -1,7 +1,7 @@
 ﻿public class Solution {
     public int MaxIncreasingSubarrays(IList<int> nums) {
         int max = 0, n = nums.Count;
-        int[] startPoint = new int[n];
+        int[] sequenceLength = new int[n];
         int prev = Int32.MaxValue;
         int length = 0;
         for(int i = 0; i < n; i++){
@@ -15,10 +15,10 @@
                 int half = length / 2;
                 max = Math.Max(max, half);
             }
-            if(i - length >= 0 && length <= startPoint[i - length]){
+            if(i - length >= 0 && length <= sequenceLength[i - length]){
                 max = Math.Max(max, length);
             }
-            startPoint[i] = length;
+            sequenceLength[i] = length;
             prev = nums[i];
         }
         return max;
