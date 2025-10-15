1130df00194e9ac63be58e47b963d3e8223817f8
Matt-Ranasinghe <matt.mailathome@gmail.com>
2025-10-15 16:39:41 +0100

    Solution for daily problem 15/10/2025

---
+++ Solutions/Normal/3350. Adjacent Increasing Subarrays Detection II/3350. Adjacent Increasing Subarrays Detection II/Solution.cs
@@ -0,0 +1,26 @@
+﻿public class Solution {
+    public int MaxIncreasingSubarrays(IList<int> nums) {
+        int max = 0, n = nums.Count;
+        int[] startPoint = new int[n];
+        int prev = Int32.MaxValue;
+        int length = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] > prev){
+                length++;
+            }
+            else{
+                length = 1;
+            }
+            if(length % 2 == 0){
+                int half = length / 2;
+                max = Math.Max(max, half);
+            }
+            if(i - length >= 0 && length <= startPoint[i - length]){
+                max = Math.Max(max, length);
+            }
+            startPoint[i] = length;
+            prev = nums[i];
+        }
+        return max;
+    }
+}
