0e963d4abc903d87d4e536620998d50884ffd330
matthew <matt.mailathome@gmail.com>
2025-10-18 21:51:36 +0100

    Daily problem in CS for 18/10/2025

---
+++ Solutions/Normal/3397. Maximum Number of Distinct Elements After Operations/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public int MaxDistinctElements(int[] nums, int k) {
+        Array.Sort(nums);
+        int result = 1;
+        int nextBest = nums[0] - k + 1;
+        for(int i = 1; i < nums.Length; i++){
+            if(nextBest <= nums[i] - k){
+                nextBest = nums[i] - k + 1;
+                result++;
+            }
+            else if(nextBest <= nums[i] + k){
+                nextBest++;
+                result++;
+            }
+        }
+        return result;
+    }
+}
