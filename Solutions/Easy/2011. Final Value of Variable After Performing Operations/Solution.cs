cd82d2f9132bbb472bd339fba11fe6088ddd0076
Matt-Ranasinghe <matt.mailathome@gmail.com>
2025-10-20 23:15:02 +0100

    Python and c# for daily problem 20/10/2025

---
+++ Solutions/Easy/2011. Final Value of Variable After Performing Operations/Solution.cs
@@ -0,0 +1,10 @@
+﻿public class Solution {
+    public int FinalValueAfterOperations(string[] operations) {
+        int res = 0;
+        foreach(string s in operations){
+            if (s.Contains('+')) res++;
+            else res--;
+        }
+        return res;
+    }
+}
