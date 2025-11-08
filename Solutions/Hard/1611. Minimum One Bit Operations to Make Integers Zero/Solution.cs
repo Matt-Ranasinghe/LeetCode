36265b1f6befab19612b5282c27b04d23536daf0
Matt-Ranasinghe <matt.mailathome@gmail.com>
2025-11-08 00:49:02 +0000

    Solution to problem 08/11/2025

---
+++ Solutions/Hard/1611. Minimum One Bit Operations to Make Integers Zero/Solution.cs
@@ -0,0 +1,25 @@
+﻿public class Solution {
+    public int MinimumOneBitOperations(int n) {
+        int result = 0;
+        
+        while (n > 0) {
+            int msb = MSB(n);
+            result ^= FlipBits(msb);
+            n ^= msb;
+        }
+        
+        return result;
+    }
+
+    private int MSB(int n) {
+        int msb = 1;
+        while (msb <= n) {
+            msb <<= 1;
+        }
+        return msb >> 1;
+    }
+    
+    private int FlipBits(int msb) {
+        return (msb << 1) - 1;
+    }
+}
