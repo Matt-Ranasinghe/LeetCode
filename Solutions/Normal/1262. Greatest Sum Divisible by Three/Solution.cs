252944d186ce7a821fd9f4fb1e7143c7c2202c20
matthew <matt.mailathome@gmail.com>
2025-11-23 22:21:42 +0000

    Solution to problem 23/11/2025

---
+++ Solutions/Normal/1262. Greatest Sum Divisible by Three/Solution.cs
@@ -0,0 +1,19 @@
+﻿public class Solution {
+    public int MaxSumDivThree(int[] nums)
+    {
+        int[] dp = new int[]{0, int.MinValue, int.MinValue};
+
+        foreach (var n in nums)
+        {
+            int[] next = (int[])dp.Clone();
+            foreach (int r in new int[]{0,1,2})
+            {
+                int nr = (r + n) % 3;
+                next[nr] = Math.Max(next[nr], dp[r] + n);
+            }
+            dp = next;
+        }
+
+        return dp[0];
+    }
+}
