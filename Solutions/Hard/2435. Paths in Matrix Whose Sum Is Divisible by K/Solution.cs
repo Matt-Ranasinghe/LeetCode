dd8917316dd00cd11759f06c63465457b0d43334
matthew <matt.mailathome@gmail.com>
2025-11-26 04:03:00 +0000

    Solutions to problems 25/11/2025 and 26/11/2025 in C#

---
+++ Solutions/Hard/2435. Paths in Matrix Whose Sum Is Divisible by K/Solution.cs
@@ -0,0 +1,31 @@
+﻿public class Solution {
+    public int NumberOfPaths(int[][] grid, int k) {
+        const int MOD = (int) 1e9 + 7;
+        int n = grid.Length, m = grid[0].Length;
+        int[,,] dp = new int[n,m,k];
+        dp[0,0,grid[0][0] % k] = 1;
+        for(int i = 1; i < n; i++){
+            for(int l = 0; l < k; l++){
+                if(dp[i - 1,0,l] == 0) continue;
+                int rem = (l + grid[i][0]) % k;
+                dp[i,0,rem] = 1;
+            }
+        }
+        for(int j = 1; j < m; j++){
+             for(int l = 0; l < k; l++){
+                if(dp[0,j - 1,l] == 0) continue;
+                int rem = (l + grid[0][j]) % k;
+                dp[0,j,rem] = 1;
+            }
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                for(int l = 0; l < k; l++){
+                    int rem = (l + grid[i][j]) % k;
+                    dp[i,j,rem] = (dp[i - 1,j,l] + dp[i,j - 1,l]) % MOD;
+                }
+            }
+        }
+        return dp[n-1,m-1,0];
+    }
+}
---
+++ Solutions/Normal/1015. Smallest Integer Divisible by K/Solution.cs
@@ -0,0 +1,13 @@
+﻿public class Solution {
+    public int SmallestRepunitDivByK(int k) {
+        if(k % 2 == 0 || k % 5 == 0){
+            return -1;
+        }
+        int counter = 1 , reminder = 1%k ;
+        while(reminder != 0){
+            reminder = (reminder * 10 + 1) % k ;
+            counter++;
+        } 
+        return counter;
+    }
+}
