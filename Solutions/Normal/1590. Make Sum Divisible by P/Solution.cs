22594bef28893d933ad958a63e90f55e04a9bdd3
Matt-Ranasinghe <matt.mailathome@gmail.com>
2025-11-30 23:42:36 +0000

    Solution to problem 30/11/2025 in C#

---
+++ Solutions/Normal/1590. Make Sum Divisible by P/Solution.cs
@@ -0,0 +1,30 @@
+﻿public class Solution {
+    public int MinSubarray(int[] nums, int p) {
+        long total = 0;
+        foreach (int x in nums) total += x;
+
+        long need = total % p;
+        if (need == 0) return 0;  // Already divisible
+
+        Dictionary<long, int> map = new Dictionary<long, int>();
+        map[0] = -1; // Prefix before the array
+
+        long prefix = 0;
+        int res = nums.Length;
+
+        for (int i = 0; i < nums.Length; i++) {
+            prefix = (prefix + nums[i]) % p;
+
+            long target = (prefix - need + p) % p;
+
+            if (map.ContainsKey(target)) {
+                res = Math.Min(res, i - map[target]);
+            }
+
+            // Update prefix index
+            map[prefix] = i;
+        }
+
+        return res == nums.Length ? -1 : res;
+    }
+}
