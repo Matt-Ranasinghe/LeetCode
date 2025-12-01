b20cbea93fce265237ceec72af12fd28fdabb2a0
matthew <matt.mailathome@gmail.com>
2025-12-01 23:04:19 +0000

    Solution to problem 01/12/2025

---
+++ Solutions/Hard/2141. Maximum Running Time of N Computers/Solution.cs
@@ -0,0 +1,32 @@
+﻿public class Solution {
+    public long MaxRunTime(int n, int[] batteries) {
+        long sum = 0;
+        foreach (int b in batteries) sum += b;
+
+        long left = 0, right = sum / n;
+        long ans = 0;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+
+            if (CanRun(mid, n, batteries)) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+    private bool CanRun(long time, int n, int[] batteries) {
+        long total = 0;
+
+        foreach (int b in batteries) {
+            total += Math.Min(b, time);
+            if (total >= (long)n * time) return true;
+        }
+
+        return total >= (long)n * time;
+    }
+}
