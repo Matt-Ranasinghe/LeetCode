7ca7925c47a57a11ac283f4257e20ebd4407fb6a
matthew <matt.mailathome@gmail.com>
2025-12-02 03:48:59 +0000

    Solution to problem 02/12/2025

---
+++ Solutions/Normal/3623. Count Number of Trapezoids I/Solution.cs
@@ -0,0 +1,20 @@
+﻿public class Solution {
+    public int CountTrapezoids(int[][] points) {
+        const int MOD = (int) 1e9 + 7;
+        Dictionary<long, (long total, int add)> levels = new Dictionary<long, (long total, int add)>();
+        foreach(int[] point in points){
+            if(!levels.ContainsKey(point[1])) levels[point[1]] = (0, 1);
+            else {
+                (long total, int add) tuple = levels[point[1]];
+                levels[point[1]] = (tuple.total + tuple.add, tuple.add + 1);
+            }
+        }
+        long result = 0;
+        long prefix = 0;
+        foreach((long total, int add) level in levels.Values){
+            result = ((result + prefix * level.total) % MOD);
+            prefix += level.total;
+        }
+        return (int) result;
+    }
+}
