2f51af81ab9e94a6f7856c25fba4a593a44ffe9d
Matthew Ranasinghe <matt.mailathome@gmial.com>
2025-12-14 23:11:00 +0000

    Solution to problem 14/12/2025 in C#

---
+++ Solutions/Hard/2147. Number of Ways to Divide a Long Corridor/Solution.cs
@@ -0,0 +1,27 @@
+﻿public class Solution {
+    public int NumberOfWays(string corridor) {
+        int chairNum = 0;
+        List<(int Start, int End)> gaps = new List<(int Start, int End)>();
+        int n = corridor.Length;
+        const int MOD = (int) 1e9 + 7;
+        int start = -1;
+        for(int i = 0; i < n; i++){
+            if(corridor[i] == 'S'){
+                chairNum++;
+                if(chairNum % 2 == 1){
+                    start = i;
+                }
+                else{
+                    gaps.Add((start, i));
+                    start = -1;
+                }
+            }
+        }
+        if(start != -1 || gaps.Count() == 0) return 0;
+        long result = 1;
+        for(int i = 0; i < gaps.Count() - 1; i++){
+            result = (result * (gaps[i + 1].Start - gaps[i].End)) % MOD;
+        }
+        return (int) result;
+    }
+}
