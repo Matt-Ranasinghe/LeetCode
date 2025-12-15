11a4e27828d05e829c3fcedd9fc928f51c6a9542
Matthew Ranasinghe <matt.mailathome@gmial.com>
2025-12-15 23:37:53 +0000

    Solution to problem 15/12/2025 in C#

---
+++ Solutions/Normal/2110. Number of Smooth Descent Periods of a Stock/Solution.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public long GetDescentPeriods(int[] prices) {
+        int start = -1, startDist = -1, current = -1;
+        int n = prices.Length;
+        long result = 0;
+        for(int i = 0; i < n; i++){
+            if(prices[i] != start - (i - startDist)){
+                result += 1;
+                startDist = i;
+                start = prices[i];
+            }
+            else{
+                result += i - startDist + 1;
+            }
+        }
+        return result;
+    }
+}
