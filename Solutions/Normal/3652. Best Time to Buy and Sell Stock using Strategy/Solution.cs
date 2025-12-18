88b2460c8ff8af510a3b25b866714d739ae54f59
Matt-Ranasinghe <matt.mailathome@gmail.com>
2025-12-18 21:47:20 +0000

    Solution to problem 18/12/2025 in C#

---
+++ Solutions/Normal/3652. Best Time to Buy and Sell Stock using Strategy/Solution.cs
@@ -0,0 +1,28 @@
+﻿public class Solution {
+    public long MaxProfit(int[] prices, int[] strategy, int k) {
+        int n = prices.Length;
+        long result = 0;
+        long baseProfit = 0, modifiedProfit = 0; 
+        int half = k/2;
+        for(int i = 0; i < n; i++){
+            baseProfit += prices[i] * strategy[i];
+        }
+        modifiedProfit = baseProfit;
+        for(int i = 0; i < k; i++){
+            if(i < half){
+                modifiedProfit -= strategy[i] * prices[i];
+            }
+            else{
+                modifiedProfit += (1 - strategy[i]) * prices[i];
+            }
+        }
+        result = Math.Max(baseProfit, modifiedProfit);
+        for(int i = 0; i < n - k; i++){
+            modifiedProfit += strategy[i] * prices[i];
+            modifiedProfit -= prices[i + half];
+            modifiedProfit += (1 - strategy[i + k]) * prices[i + k];
+            result = Math.Max(modifiedProfit, result);
+        }
+        return result;
+    }
+}
