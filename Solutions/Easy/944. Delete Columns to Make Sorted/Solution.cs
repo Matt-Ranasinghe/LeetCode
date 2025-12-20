1a943bdbd2317b1c3548118011f1fadd06a1bb78
Matt-Ranasinghe <matt.mailathome@gmail.com>
2025-12-20 23:21:30 +0000

    Solution to problemmm 20/12/2025 in C#

---
+++ Solutions/Easy/944. Delete Columns to Make Sorted/Solution.cs
@@ -0,0 +1,16 @@
+﻿public class Solution {
+    public int MinDeletionSize(string[] strs) {
+        int n = strs.Length, m = strs[0].Length, result = 0;
+        for(int i = 0; i < m; i++){
+            char prev = '0';
+            for(int j = 0; j < n; j++){
+                if(prev.CompareTo(strs[j][i]) > 0){
+                    result++;
+                    break;
+                }
+                prev = strs[j][i];
+            }
+        }
+        return result;
+    }
+}
