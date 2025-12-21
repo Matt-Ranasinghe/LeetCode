d022b847045e299bf7a86174b7f7d02cdc82b4a0
matthew <matt.mailathome@gmail.com>
2025-12-21 02:30:53 +0000

    Solution to problem 21/12/2025 in C#

---
+++ Solutions/Normal/955. Delete Columns to Make Sorted II/Solution.cs
@@ -0,0 +1,35 @@
+﻿public class Solution {
+    public int MinDeletionSize(string[] strs) {
+        List<List<string>> breakDownSeq = new List<List<string>>(), nextSeq = new List<List<string>>();
+        breakDownSeq.Add(strs.ToList());
+        int result = 0, n = strs[0].Length;
+        for(int i = 0; i < n; i++){
+            bool fail = false;
+            foreach(List<string> seq in breakDownSeq){
+                nextSeq.Add(new List<string>());
+                nextSeq[nextSeq.Count - 1].Add(seq[0]);
+                for(int j = 1; j < seq.Count; j++){
+                    if(seq[j][i] < seq[j - 1][i]){
+                        result++;
+                        fail = true;
+                        break;
+                    }
+                    else if(seq[j][i] > seq[j - 1][i]){
+                        nextSeq.Add(new List<string>());
+                    }
+                    nextSeq[nextSeq.Count - 1].Add(seq[j]);
+                }
+                if(fail) break;
+            }
+            if(!fail){
+                breakDownSeq = nextSeq;
+                nextSeq = new List<List<string>>();
+            }
+            else{
+                nextSeq.Clear();
+            }
+            if(breakDownSeq.Count == strs.Length) return result;
+        }
+        return result;
+    }
+}
