9c8e21efd73d6697e679901853d4a8ca3bddf702
Matt-Ranasinghe <matt.mailathome@gmail.com>
2025-12-23 01:45:49 +0000

    Solution to problem 23/12/2025 in C#

--- Solutions/Hard/2092. Find All People With Secret/Solution.cs
+++ Solutions/Hard/2092. Find All People With Secret/Solution.cs
@@ -39,4 +39,4 @@
         }
         return knows.ToList();
     }
-}
+} 
---
+++ Solutions/Normal/2054. Two Best Non-Overlapping Events/Solution.cs
@@ -0,0 +1,15 @@
+﻿public class Solution {
+    public int MaxTwoEvents(int[][] events) {
+        Array.Sort(events, (x, y) => x[0].CompareTo(y[0]));
+        int result = 0, max = 0, n = events.Length;
+        PriorityQueue<(int end, int score), int> pq = new PriorityQueue<(int end, int score), int>();
+        foreach(int[] ev in events){
+            pq.Enqueue((ev[1], ev[2]), ev[1]);
+            while(pq.Count > 0 && pq.Peek().end < ev[0]){
+                max = Math.Max(pq.Dequeue().score, max);
+            }
+            result = Math.Max(result, max + ev[2]);
+        }
+        return result;
+    }
+}
