03029a8ef29d757c326f86b7c4274d2d8faaef42
Matt-Ranasinghe <matt.mailathome@gmail.com>
2025-12-24 00:59:32 +0000

    Solution to problem 24/12/2025 in C#

---
+++ Solutions/Easy/3074. Apple Redistribution into Boxes/Solution.cs
@@ -0,0 +1,12 @@
+﻿public class Solution {
+    public int MinimumBoxes(int[] apple, int[] capacity) {
+        int appleSum = apple.Sum();
+        Array.Sort(capacity, (x, y) => y.CompareTo(x));
+        int result = 0, count = 0;
+        while(appleSum > 0) {
+            appleSum -= capacity[count++];
+            result++;
+        }
+        return result;
+    }
+}
