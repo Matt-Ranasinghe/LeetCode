b0ca692475fbd46a1eee8e2098fdf65f48bd69c9
matthew <matt.mailathome@gmail.com>
2025-12-25 01:00:38 +0000

    Solution to problem 25/12/2025 in C# (Merry Christmas).

---
+++ Solutions/Normal/3075. Maximize Happiness of Selected Children/Solution.cs
@@ -0,0 +1,11 @@
+﻿public class Solution {
+    public long MaximumHappinessSum(int[] happiness, int k) {
+        Array.Sort(happiness, (x, y) => y.CompareTo(x));
+        long result = 0;
+        for(int i = 0; i < k; i++){
+            if(happiness[i] - i <= 0) return result;
+            result += happiness[i] - i;
+        }
+        return result;
+    }
+}
