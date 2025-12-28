a1ba070b9263c6d73b9edb30bdf2af3978d3fc5c
matthew <matt.mailathome@gmail.com>
2025-12-28 00:33:36 +0000

    Solution to problem 28/12/2025 in C#

---
+++ Solutions/Easy/1351. Count Negative Numbers in a Sorted Matrix/Solution.cs
@@ -0,0 +1,24 @@
+﻿public class Solution {
+    public int CountNegatives(int[][] grid) {
+        int n = grid[0].Length, result = 0;
+        foreach(int[] row in grid){
+            result += BinarySearch(row, n);
+        }
+        return result;
+    }
+
+    private int BinarySearch(int[] row, int n){
+        int left = 0, right = n - 1, res = -1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if (row[mid] >= 0){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+                res = mid;
+            }
+        }
+        return res == -1 ? 0 : n - res;
+    }
+}
