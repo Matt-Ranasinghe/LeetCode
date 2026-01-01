03fc295098073bfe39b8082efba2aedb173b1969
Matt-Ranasinghe <matt.mailathome@gmail.com>
2026-01-01 02:18:55 +0000

    Solution to leetcode problem 66 (01/12/2026) in C#

---
+++ Solutions/Easy/66. Plus One/Solution.cs
@@ -0,0 +1,16 @@
+﻿public class Solution {
+    public int[] PlusOne(int[] digits) {
+        int n = digits.Length, add = n - 1;
+        while(digits[add] == 9){
+            digits[add] = 0;
+            add--;
+            if(add == -1){
+                int[] ld = new int[n + 1];
+                ld[0] = 1;
+                return ld;
+            }
+        }
+        digits[add]++;
+        return digits;
+    }
+}
