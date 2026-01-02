5bace8c398fe163e772d4ea7fce80932d58d0628
Matt-Ranasinghe <matt.mailathome@gmail.com>
2026-01-02 01:06:49 +0000

    Solution to leetcode problem 961 (02/01/2026) in C#

---
+++ Solutions/Easy/961. N-Repeated Element in Size 2N Array/Solution.cs
@@ -0,0 +1,10 @@
+﻿public class Solution {
+    public int RepeatedNTimes(int[] nums) {
+        HashSet<int> seen = new HashSet<int>();
+        foreach(int num in nums){
+            if(!seen.Contains(num)) seen.Add(num);
+            else return num;
+        }
+        return -1;
+    }
+}
