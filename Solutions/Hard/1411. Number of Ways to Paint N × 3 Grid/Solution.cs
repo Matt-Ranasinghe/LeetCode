92fcd56d667a94a17b213af1ac90ff52af10514b
Matthew Ranasinghe <matt.mailathome@gmial.com>
2026-01-03 00:46:33 +0000

    Solution to leetcode problem 1411 (03/01/2026) in C#

---
+++ Solutions/Hard/1411. Number of Ways to Paint N × 3 Grid/Solution.cs
@@ -0,0 +1,17 @@
+﻿public class Solution {
+    private const int MOD = (int) 1e9 + 7;
+
+    public int NumOfWays(int n) {
+        long same = 6;
+        long diff = 6;
+
+        for (int i = 1; i < n; i++) {
+            long newSame = (same * 3 + diff * 2) % MOD;
+            long newDiff = (same * 2 + diff * 2) % MOD;
+            same = newSame;
+            diff = newDiff;
+        }
+
+        return (int)((same + diff) % MOD);
+    }
+}
