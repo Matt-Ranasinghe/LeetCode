33411198cf5334357e9e4ae610aacaf1816924f8
Matt-Ranasinghe <matt.mailathome@gmail.com>
2026-01-05 16:20:11 +0000

    Changed file name to medium and solution to leetcode problem 1975 (05/01/2026) in C#

---
+++ Solutions/Medium/1975. Maximum Matrix Sum/Solution.cs
@@ -0,0 +1,17 @@
+﻿public class Solution {
+    public long MaxMatrixSum(int[][] matrix) {
+        int smallestNum = Int32.MaxValue;
+        bool oddNeg = false;
+        long result = 0;
+        foreach(int[] row in matrix){
+            foreach(int num in row){
+                if(num < 0) {
+                    oddNeg = !oddNeg;
+                }
+                smallestNum = Math.Min(smallestNum, Math.Abs(num));
+                result += Math.Abs(num);
+            }
+        }
+        return oddNeg ? result - 2 * smallestNum : result;
+    }
+}
