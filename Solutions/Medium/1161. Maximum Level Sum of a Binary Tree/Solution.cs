1efa2d58bf4bfd081c575fa103a669457cb85ffb
Matt-Ranasinghe <matt.mailathome@gmail.com>
2026-01-06 00:50:53 +0000

    Solution to leetcode problem 1161 (06/01/2026) in C#

---
+++ Solutions/Medium/1161. Maximum Level Sum of a Binary Tree/Solution.cs
@@ -0,0 +1,36 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MaxLevelSum(TreeNode root) {
+        int maxSum = Int32.MinValue, curSum = 0, depth = 0, result = 0;
+        Queue<(TreeNode node, int depth)> queue = new Queue<(TreeNode node, int depth)>();
+        queue.Enqueue((root, depth));
+        while(queue.Count > 0){
+            if(queue.Peek().depth != depth){
+                if(maxSum < curSum){
+                    maxSum = curSum;
+                    result = depth + 1;
+                }
+                depth++;
+                curSum = 0;
+            }
+            (TreeNode node, int depth) value = queue.Dequeue();
+            curSum += value.node.val;
+            if(value.node.left != null) queue.Enqueue((value.node.left, depth + 1));
+            if(value.node.right != null) queue.Enqueue((value.node.right, depth + 1));
+        }
+        if(maxSum < curSum) result = depth + 1;
+        return result;
+    }
+}
