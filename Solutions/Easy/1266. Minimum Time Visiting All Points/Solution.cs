98991292462c169ac70df235697fb354d5198563
matthew <matt.mailathome@gmail.com>
2026-01-12 01:06:45 +0000

    Solution to leetcode problem 1266 in C#

---
+++ Solutions/Easy/1266. Minimum Time Visiting All Points/Solution.cs
@@ -0,0 +1,13 @@
+﻿public class Solution {
+    public int MinTimeToVisitAllPoints(int[][] points) {
+        int prev_x = points[0][0], prev_y = points[0][1];
+        int res = 0;
+        for(int i = 1; i < points.Length; i++)
+        {
+            res += Math.Max(Math.Abs(prev_x - points[i][0]), Math.Abs(prev_y - points[i][1]));
+            prev_x = points[i][0];
+            prev_y = points[i][1];
+        }
+        return res;
+    }
+}
