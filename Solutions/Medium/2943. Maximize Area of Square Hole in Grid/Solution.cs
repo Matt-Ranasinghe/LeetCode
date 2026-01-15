623350d521c55315b3181cca20bde18fe558d360
matthew <matt.mailathome@gmail.com>
2026-01-15 22:08:59 +0000

    Solution to leetcode problem 2943 in C#

---
+++ Solutions/Medium/2943. Maximize Area of Square Hole in Grid/Solution.cs
@@ -0,0 +1,34 @@
+﻿public class Solution {
+    public int MaximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
+        Array.Sort(hBars);
+        Array.Sort(vBars);
+        int maxHeight = 0, maxWidth = 0;
+        int prev = -1, current = 1;
+        foreach(int bar in hBars){
+            if(prev == bar - 1){
+                current++;
+            }
+            else{
+                maxHeight = Math.Max(maxHeight, current);
+                current = 2;
+            }
+            prev = bar;
+        }
+        maxHeight = Math.Max(maxHeight, current);
+        current = 1;
+        prev = -1;
+        foreach(int bar in vBars){
+            if(prev == bar - 1){
+                current++;
+            }
+            else{
+                maxWidth = Math.Max(maxWidth, current);
+                current = 2;
+            }
+            prev = bar;
+        }
+        maxWidth = Math.Max(maxWidth, current);
+        return SquareNum(Math.Min(maxWidth, maxHeight));
+    }
+    private int SquareNum(int x) => x * x;
+}
