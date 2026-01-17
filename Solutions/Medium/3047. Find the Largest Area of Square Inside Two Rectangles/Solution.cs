ad08fceb430d876359c8a3386a6c8fdc6e52b5e9
matthew <matt.mailathome@gmail.com>
2026-01-17 01:53:52 +0000

    Solution to leetcode problem 3047 in C#

---
+++ Solutions/Medium/3047. Find the Largest Area of Square Inside Two Rectangles/Solution.cs
@@ -0,0 +1,20 @@
+﻿public class Solution {
+    public long LargestSquareArea(int[][] bottomLeft, int[][] topRight) {
+        int n = bottomLeft.Length;
+        long result = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                result = Math.Max(result, maxSideLength(bottomLeft[i], bottomLeft[j], topRight[i], topRight[j]));
+            }
+        }
+        return result * result;
+    }
+
+    private int maxSideLength(int[] BL1, int[] BL2, int[] TR1, int[] TR2){
+        int BX = Math.Max(BL1[0], BL2[0]);
+        int BY = Math.Max(BL1[1], BL2[1]);
+        int TX = Math.Min(TR1[0], TR2[0]);
+        int TY = Math.Min(TR1[1], TR2[1]);
+        return Math.Min((TX - BX), (TY - BY));
+    }
+}
