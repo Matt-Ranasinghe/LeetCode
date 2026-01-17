fdcc572407dda8d6ac9f34086192957ec62b5a29
matthew <matt.mailathome@gmail.com>
2026-01-17 01:54:18 +0000

    Function name change

--- Solutions/Medium/3047. Find the Largest Area of Square Inside Two Rectangles/Solution.cs
+++ Solutions/Medium/3047. Find the Largest Area of Square Inside Two Rectangles/Solution.cs
@@ -4,13 +4,13 @@
         long result = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < i; j++){
-                result = Math.Max(result, maxSideLength(bottomLeft[i], bottomLeft[j], topRight[i], topRight[j]));
+                result = Math.Max(result, MaxSideLength(bottomLeft[i], bottomLeft[j], topRight[i], topRight[j]));
             }
         }
         return result * result;
     }
 
-    private int maxSideLength(int[] BL1, int[] BL2, int[] TR1, int[] TR2){
+    private int MaxSideLength(int[] BL1, int[] BL2, int[] TR1, int[] TR2){
         int BX = Math.Max(BL1[0], BL2[0]);
         int BY = Math.Max(BL1[1], BL2[1]);
         int TX = Math.Min(TR1[0], TR2[0]);
