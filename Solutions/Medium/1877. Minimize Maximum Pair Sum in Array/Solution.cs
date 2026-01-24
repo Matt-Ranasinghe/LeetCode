099b31e9a87faa9946009ad1a50de9a2adaa083b
matthew <matt.mailathome@gmail.com>
2026-01-24 17:20:56 +0000

    Solution to leetcode problem 1877 in C#.

---
+++ Solutions/Medium/1877. Minimize Maximum Pair Sum in Array/Solution.cs
@@ -0,0 +1,10 @@
+﻿public class Solution {
+    public int MinPairSum(int[] nums) {
+        Array.Sort(nums);
+        int result = Int32.MinValue, n = nums.Length;
+        for(int i = 0; i < n / 2; i++){
+            result = Math.Max(nums[i] + nums[n - i - 1], result);
+        }
+        return result;
+    }
+}
