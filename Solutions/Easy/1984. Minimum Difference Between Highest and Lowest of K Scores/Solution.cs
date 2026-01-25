e736aaac9b6820b5f234d233772d871e5bffa82c
matthew <matt.mailathome@gmail.com>
2026-01-25 12:58:34 +0000

    Solution to leetcode problem 1984 in C# and python

---
+++ Solutions/Easy/1984. Minimum Difference Between Highest and Lowest of K Scores/Solution.cs
@@ -0,0 +1,11 @@
+﻿public class Solution {
+    public int MinimumDifference(int[] nums, int k) {
+        if(k == 1) return 0;
+        Array.Sort(nums);
+        int result = Int32.MaxValue;
+        for(int i = 0; i < nums.Length - (k - 1); i++){
+            result = Math.Min(result, nums[i + k - 1] - nums[i]);
+        }
+        return result;
+    }
+}
