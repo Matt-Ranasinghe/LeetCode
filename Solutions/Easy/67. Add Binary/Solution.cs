68a33a30002ff861646f066712143b81dfa1127b
matthew <matt.mailathome@gmail.com>
2026-02-15 03:34:00 +0000

    Solution to leetcode problem 799 and 67 in C#.

---
+++ Solutions/Easy/67. Add Binary/Solution.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public string AddBinary(string a, string b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.Length - 1;
+        int j = b.Length - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+            sb.Append(sum % 2);
+            carry = sum / 2;
+        }
+        char[] charArray = sb.ToString().ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
