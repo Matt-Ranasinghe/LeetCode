bf602ec75d88a68fcab00a9bd3c9b996b8563929
matthew <matt.mailathome@gmail.com>
2026-02-23 02:30:19 +0000

    Solution to leetcode problem 1461 in C#

---
+++ Solutions/Medium/1461. Check If a String Contains All Binary Codes of Size K/Solution.cs
@@ -0,0 +1,13 @@
+﻿public class Solution {
+    public bool HasAllCodes(string s, int k) {
+        HashSet<string> seen = new HashSet<string>();
+        int n = s.Length;
+        int expectedCount = (int)Math.Pow(2,k);
+        for(int i = 0; i < n - k + 1; i++){
+            string substring = s[i..(k + i)];
+            seen.Add(substring);
+            if(expectedCount == seen.Count) return true;
+        }
+        return false;
+    }
+}
