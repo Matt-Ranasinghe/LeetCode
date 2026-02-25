a316af746aa6c640492d9277e758d3d61b08e811
matthew <matt.mailathome@gmail.com>
2026-02-25 00:53:32 +0000

    Solution to leetcode problem 1356 in C#.

---
+++ Solutions/Easy/1356. Sort Integers by The Number of 1 Bits/Solution.cs
@@ -0,0 +1,23 @@
+﻿public class Solution {
+    public int[] SortByBits(int[] arr) {
+        PriorityQueue<int, (int, int)> pq = new PriorityQueue<int, (int, int)>();
+        foreach(int num in arr){
+            int bitCount = CountBits(num);
+            pq.Enqueue(num, (bitCount, num));
+        }
+        int n = arr.Length;
+        for(int i = 0; i < n; i++){
+            arr[i] = pq.Dequeue();
+        }
+        return arr;
+    }
+
+    private int CountBits(int num){
+        int result = 0;
+        while(num > 0){
+            result += (num & 1);
+            num >>= 1;
+        }
+        return result;
+    }
+}
