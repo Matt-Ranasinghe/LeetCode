6dc08d4cefb0ff00c3420561340c7cfc9b181e18
matthew <matt.mailathome@gmail.com>
2026-03-06 19:06:04 +0000

    Solution to leetcode problem 1784 in C#.

---
+++ Solutions/Easy/1784. Check if Binary String Has at Most One Segment of Ones/Solution.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public bool CheckOnesSegment(string s) {
+        bool oneSeen = false, halted = false;
+        foreach(char c in s){
+            if(c == '1') 
+            {
+                oneSeen = true;
+                if(halted) return false;
+            }
+            else if(oneSeen) halted = true;
+        }
+        return true;
+    }
+}
