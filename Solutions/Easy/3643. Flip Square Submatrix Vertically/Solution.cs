5a4cb741acc5291741504d2ef61ea61592bc6d6d
matthew <matt.mailathome@gmail.com>
2026-03-21 21:26:36 +0000

    Solution to leetcode problem 3643 in C#.

---
+++ Solutions/Easy/3643. Flip Square Submatrix Vertically/Solution.cs
@@ -0,0 +1,13 @@
+﻿public class Solution {
+    public int[][] ReverseSubmatrix(int[][] grid, int x, int y, int k) {
+        for (int j = x; j < x + k / 2; j++) {
+            int mirrorJ = (x + (x + k - 1)) - j;
+            for (int i = y; i < y + k; i++) {
+                int temp = grid[j][i];
+                grid[j][i] = grid[mirrorJ][i];
+                grid[mirrorJ][i] = temp;
+            }
+        }
+        return grid;
+    }
+}
