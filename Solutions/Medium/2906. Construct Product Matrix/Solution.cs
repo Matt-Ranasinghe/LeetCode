f1855b8862bd6b0de123c6a33a1195edf7bedb6b
matthew <matt.mailathome@gmail.com>
2026-03-24 23:46:54 +0000

    Solution to leetcode problem 2906 in C#.

---
+++ Solutions/Medium/2906. Construct Product Matrix/Solution.cs
@@ -0,0 +1,34 @@
+﻿public class Solution {
+    public int[][] ConstructProductMatrix(int[][] grid) {
+        int n = grid.Length, m = grid[0].Length;
+        int[,] prefixSum = new int[n * m, 2];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                int reverse = (n - i - 1) * m + (m - j - 1);
+                if(i == 0 && j == 0){
+                    prefixSum[0, 0] = grid[0][0] % 12345;
+                    prefixSum[reverse, 1] = grid[n - 1][m - 1] % 12345;
+                }
+                else{
+                    int mult = i * m + j;
+                    prefixSum[mult, 0] = (int)((long) prefixSum[mult - 1, 0] * (long)grid[i][j] % 12345);
+                    prefixSum[reverse, 1] = (int)((long)prefixSum[reverse + 1, 1] * (long)grid[n - i - 1][m - j - 1] % 12345);
+                }
+            }
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(i == 0 && j == 0){
+                    grid[i][j] = prefixSum[i * m + j + 1, 1] % 12345;
+                }
+                else if(i == n - 1 && j == m - 1){
+                    grid[i][j] = prefixSum[i * m + j - 1, 0] % 12345;
+                }
+                else{
+                    grid[i][j] = (int)((long)prefixSum[i * m + j - 1, 0] * (long)prefixSum[i * m + j + 1, 1]  % 12345);
+                }
+            }
+        }
+        return grid;
+    }
+}
