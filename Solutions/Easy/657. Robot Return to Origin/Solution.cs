15dd9f05341a51d391858d017a38bb148f71c982
matthew <matt.mailathome@gmail.com>
2026-04-05 19:29:19 +0100

    Solution to leetcode problem 657 in C#.

---
+++ Solutions/Easy/657. Robot Return to Origin/Solution.cs
@@ -0,0 +1,12 @@
+﻿public class Solution {
+    public bool JudgeCircle(string moves) {
+        int up = 0, down = 0, left = 0, right = 0;
+        foreach(char c in moves){
+            if(c == 'U') up++;
+            else if(c == 'D') down++;
+            else if(c == 'L') left++;
+            else right++;
+        }
+        return up == down && left == right;
+    }
+}
