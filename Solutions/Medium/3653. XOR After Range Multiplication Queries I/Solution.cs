70617cf42d2d9525e9425c8629f402562b8cc00b
Matt-Ranasinghe <matt.mailathome@gmail.com>
2026-04-08 21:40:42 +0100

    Solution to leetcode problem 3653 in C#.

---
+++ Solutions/Medium/3653. XOR After Range Multiplication Queries I/Solution.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public int XorAfterQueries(int[] nums, int[][] queries) {
+        int result = 0, MOD = (int) 1e9 + 7;
+        foreach(int[] query in queries){
+            for(int i = query[0]; i <= query[1]; i += query[2]){
+                nums[i] = (int) (((long) nums[i] * query[3]) % MOD);
+            }
+        }
+        foreach(int num in nums){
+            result ^= num;
+        }
+        return result;
+    }
+}
