e4d838a86b05d7566d4b79112fcd67a54cc3dde4
matthew <matt.mailathome@gmail.com>
2026-04-13 16:59:42 +0100

    Solution to leetcode problem 1848 in C#.

---
+++ Solutions/Easy/1848. Minimum Distance to the Target Element/Solution.cs
@@ -0,0 +1,11 @@
+﻿public class Solution {
+    public int GetMinDistance(int[] nums, int target, int start) {
+        int result = nums.Length - 1;
+        for (int i = 0; i < nums.Length; i++) {
+            result = nums[i] == target ?
+                    (result > Math.Abs(i - start) ? Math.Abs(i - start) : result) :
+                    result;
+        }
+        return result;
+    }
+}
