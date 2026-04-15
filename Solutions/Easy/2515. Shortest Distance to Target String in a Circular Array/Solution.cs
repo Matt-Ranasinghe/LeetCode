46212908afc16ec45f11a6f845f5254f615b7aad
Matt-Ranasinghe <matt.mailathome@gmail.com>
2026-04-15 19:23:47 +0100

    Solution to leetcode problem 2515 in C#.

---
+++ Solutions/Easy/2515. Shortest Distance to Target String in a Circular Array/Solution.cs
@@ -0,0 +1,10 @@
+﻿public class Solution {
+    public int ClosestTarget(string[] words, string target, int startIndex) {
+        int n = words.Length;
+        for(int i = 0; i <= n / 2; i++){
+            if(words[(startIndex - i + n) % n] == target) return i;
+            if(words[(startIndex + i) % n] == target) return i;
+        }
+        return -1;
+    }
+}
