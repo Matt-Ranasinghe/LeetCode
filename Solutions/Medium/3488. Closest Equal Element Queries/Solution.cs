ce2de777e67c0473ecf6b685a1395285a07899c9
Matt-Ranasinghe <matt.mailathome@gmail.com>
2026-04-16 01:48:17 +0100

    Solution to leetcode problem 3488 in C#.

---
+++ Solutions/Medium/3488. Closest Equal Element Queries/Solution.cs
@@ -0,0 +1,34 @@
+﻿public class Solution {
+    public IList<int> SolveQueries(int[] nums, int[] queries) {
+        int n = nums.Length;
+        int[] distances = new int[n];
+        Array.Fill(distances, Int32.MaxValue);
+        Dictionary<int, int> firstIndex = new Dictionary<int, int>();
+        Dictionary<int, int> lastIndex = new Dictionary<int, int>();
+        for(int i = 0; i < n; i++){
+            int num = nums[i];
+            if(lastIndex.ContainsKey(num)){
+                int prev = lastIndex[num];
+                distances[i] = i - prev;
+                distances[prev] = Math.Min(distances[i], distances[prev]);
+            }
+            else{
+                firstIndex[num] = i;
+            }
+            lastIndex[num] = i;
+        }
+        foreach(KeyValuePair<int, int> fi in firstIndex){
+            int li = lastIndex[fi.Key];
+            if(fi.Value == li) distances[li] = -1;
+            else{
+                distances[li] = Math.Min(fi.Value + n - li, distances[li]);
+                distances[fi.Value] = Math.Min(fi.Value + n - li, distances[fi.Value]);
+            }
+        }
+        IList<int> results = new List<int>();
+        foreach(int query in queries){
+            results.Add(distances[query]);
+        }
+        return results;
+    }
+}
