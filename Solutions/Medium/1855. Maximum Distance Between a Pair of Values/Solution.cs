58e0e53626369af6d03df04539734e79c22f0ba7
Matt-Ranasinghe <matt.mailathome@gmail.com>
2026-04-19 01:43:08 +0100

    Solution to leetcode problem 1855 in C#.

---
+++ Solutions/Medium/1855. Maximum Distance Between a Pair of Values/Solution.cs
@@ -0,0 +1,12 @@
+﻿public class Solution {
+    public int MaxDistance(int[] nums1, int[] nums2) {
+        int n = nums1.Length, m = nums2.Length, result = 0;
+        int highestPrev = 0, right = 0;
+        for(int i = 0; i < n; i++){
+            if(right == m) return result;
+            while(right < m && nums2[right] >= nums1[i]) right++;
+            result = Math.Max(result, right - i - 1);
+        }
+        return result;
+    }
+}
