9e537deff4e885355227be808de78c45c48c1ae8
matthew <matt.mailathome@gmail.com>
2026-05-02 00:47:27 +0100

    Solution to leetcode problem 396 in C#.

---
+++ Solutions/Medium/396. Rotate Function/Solution.cs
@@ -0,0 +1,19 @@
+﻿public class Solution {
+    public int MaxRotateFunction(int[] nums) {
+        int n = nums.Length;
+        int sum = 0;
+        int R = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += nums[i];
+            R += i * nums[i];
+        }
+        int res = R;
+        for (int j = 1; j < n; j++)
+        {
+            R += sum - n * nums[n - j];
+            res = Math.Max(res, R);
+        }
+        return res;
+    }
+}
