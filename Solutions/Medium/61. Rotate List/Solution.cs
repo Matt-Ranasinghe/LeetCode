870dc5cfde6fff2784ee2e63b9366cf69b045f0c
Matt-Ranasinghe <matt.mailathome@gmail.com>
2026-05-05 14:48:15 +0100

    Solution to leetcode problem 61 in C#.

---
+++ Solutions/Medium/61. Rotate List/Solution.cs
@@ -0,0 +1,34 @@
+﻿
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+ 
+public class Solution {
+    public ListNode RotateRight(ListNode head, int k) {
+        if(k == 0 || head == null || head.next == null) return head;
+        ListNode headCopy = head;
+        int listLength = 0;
+        while(headCopy != null){
+            headCopy = headCopy.next;
+            listLength++;
+        }
+        int rotation = k % listLength;
+        headCopy = head;
+        for(int i = 0; i < listLength - rotation - 1; i++){
+            headCopy = headCopy.next;
+        }
+        ListNode tail = headCopy.next;
+        headCopy.next = null;
+        ListNode tailCopy = tail;
+        while(tailCopy.next != null){
+            tailCopy = tailCopy.next;
+        }
+        tailCopy.next = head;
+        return tail;
+    }
+}
