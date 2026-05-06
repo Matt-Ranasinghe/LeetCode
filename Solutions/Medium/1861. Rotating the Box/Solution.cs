75a49961bf9732d3f68ea5d3085c4243fc9f7284
Matt-Ranasinghe <matt.mailathome@gmail.com>
2026-05-06 19:56:50 +0100

    Solution to leetcode problem 1861 in C#.

---
+++ Solutions/Medium/1861. Rotating the Box/Solution.cs
@@ -0,0 +1,23 @@
+﻿public class Solution {
+    public char[][] RotateTheBox(char[][] boxGrid) {
+        int n = boxGrid.Length, m = boxGrid[0].Length;
+        char[][] result = new char[m][];
+        for(int i = 0; i < m; i++){
+            result[i] = new char[n];
+            Array.Fill(result[i], '.');
+        }
+        for(int i = 0; i < n; i++){
+            int pointer = m - 1;
+            for(int j = m - 1; j >= 0; j--){
+                if(boxGrid[i][j] == '#'){
+                    result[pointer--][n - i - 1] = '#';
+                }
+                else if(boxGrid[i][j] == '*'){
+                    result[j][n - i - 1] = '*';
+                    pointer = j - 1;
+                }
+            }
+        }
+        return result;
+    }
+}
