a775f75c1107568827b48fad299f0c4a86e03abf
matthew <matt.mailathome@gmail.com>
2026-05-10 18:04:07 +0100

    Solution to leetcode problem 2770 in C#.

---
+++ Solutions/Medium/2770. Maximum Number of Jumps to Reach the Last Index/Solution.cs
@@ -0,0 +1,21 @@
+﻿public class Solution {
+    public int MaximumJumps(int[] nums, int target) {
+        int n = nums.Length;
+        int[] dp = new int[n];
+        for(int i = 1; i < n; i++){
+            dp[i] = -1;
+        }
+        int result = -1;
+        for(int i = 0; i < n; i++){
+            if(dp[i] == -1) continue;
+            int currentNum = nums[i];
+            for(int j = i + 1; j < n; j++){
+                int nextNum = nums[j];
+                if((int)Math.Abs(currentNum - nextNum) <= target){
+                    dp[j] = Math.Max(dp[i] + 1, dp[j]);
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+}
