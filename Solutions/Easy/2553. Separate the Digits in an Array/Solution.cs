5c7f38d4b994223088803e9d8380ab92c0627c13
matthew <matt.mailathome@gmail.com>
2026-05-11 19:20:28 +0100

    Solution to leetcode problem 2553 in C#.

---
+++ Solutions/Easy/2553. Separate the Digits in an Array/Solution.cs
@@ -0,0 +1,15 @@
+﻿public class Solution {
+    public int[] SeparateDigits(int[] nums) {
+        List<int> result = new List<int>();
+        int n = nums.Length;
+        for(int i = n - 1; i >= 0; i--){
+            int num = nums[i];
+            while(num > 0){
+                result.Add(num % 10);
+                num /= 10;
+            }
+        }
+        result.Reverse();
+        return result.ToArray();
+    }
+}
