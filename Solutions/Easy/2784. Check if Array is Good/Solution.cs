56d79259a3b7ac621e040115965f842d94be5d4e
matthew <matt.mailathome@gmail.com>
2026-05-14 16:02:15 +0100

    Solution to leetcode problem 2784 in C#.

---
+++ Solutions/Easy/2784. Check if Array is Good/Solution.cs
@@ -0,0 +1,19 @@
+﻿public class Solution {
+    public bool IsGood(int[] nums) {
+        int n = nums.Length;
+        int[] seen = new int[n];
+        foreach(int num in nums){
+
+            if(num >= n) return false;
+            if(num == n - 1){
+                if(seen[num] > 1) return false;
+            }
+            else if(seen[num] > 0) return false;
+            seen[num]++;
+        }
+        for(int i = 1; i < n; i++){
+            if(seen[i] == 0) return false;
+        }
+        return true;
+    }
+}
