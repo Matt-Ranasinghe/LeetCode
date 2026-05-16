009774751d864e6c2a9e1b256e1ea1ce48034938
matthew <matt.mailathome@gmail.com>
2026-05-16 16:59:02 +0100

    Solution to leetcode problem 154 in C#.

---
+++ Solutions/Hard/154. Find Minimum in Rotated Sorted Array II/Solution.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public int FindMin(int[] nums) {
+        int n = nums.Length;
+        int left = 0, right = n - 1;
+        if(nums[left] < nums[right]) return nums[0];
+        while(left < right){
+            int mid = (right - left) / 2 + left;
+            if(nums[mid] < nums[right]){
+                right = mid;
+            }
+            else if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            }
+            else right--;
+        }
+        return nums[left];
+    }
+}
