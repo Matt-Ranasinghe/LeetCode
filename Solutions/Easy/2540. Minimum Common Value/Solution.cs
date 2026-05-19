b65ac76dd50f5a64dc617522e06a250bd5d48610
matthew <matt.mailathome@gmail.com>
2026-05-19 13:36:25 +0100

    Solution to leetcode problem 2540 in C#.

---
+++ Solutions/Easy/2540. Minimum Common Value/Solution.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public int GetCommon(int[] nums1, int[] nums2) {
+        int n = nums1.Length, m = nums2.Length;
+        int pointer1 = 0, pointer2 = 0;
+        while(pointer1 < n && pointer2 < m){
+            int num1 = nums1[pointer1];
+            int num2 = nums2[pointer2];
+            if(num1 == num2) return num1;
+            else if(num1 > num2) pointer2++;
+            else pointer1++;
+        }
+        return -1;
+    }
+}
