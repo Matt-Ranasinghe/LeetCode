a36c599c5cb9cc5c9f27454dffc69f76a96250cc
matthew <matt.mailathome@gmail.com>
2026-05-23 01:13:53 +0100

    Solution to leetcode problem 1752 in C#.

---
+++ Solutions/Easy/1752. Check if Array Is Sorted and Rotated/Solution.cs
@@ -0,0 +1,19 @@
+﻿public class Solution {
+    public bool Check(int[] nums) {
+        bool drop = false;
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(i == nums.Length - 1)
+            {
+                if(!(nums[0] >= nums[i]) && drop) return false;
+                else if(!(nums[0] >= nums[i])) drop = true;
+            }
+            else
+            {
+                if(!(nums[i + 1] >= nums[i]) && drop) return false;
+                else if(!(nums[i + 1] >= nums[i])) drop = true;
+            }
+        }
+        return true;
+    }
+}
