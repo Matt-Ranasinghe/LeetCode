e10ddab436a6b2dbc382bd5cd1dc66fb0da9faf5
matthew <matt.mailathome@gmail.com>
2026-05-25 13:20:40 +0100

    Solution to leetcode problem 1871 in C#.

---
+++ Solutions/Medium/1871. Jump Game VII/Solution.cs
@@ -0,0 +1,23 @@
+﻿public class Solution {
+    public bool CanReach(string s, int minJump, int maxJump) {
+        int n = s.Length;
+        if(s[n - 1] == '1') return false;
+        bool[] visited = new bool[n];
+        visited[0] = true;
+        Queue<int> queue = new Queue<int>();
+        queue.Enqueue(0);
+        int right = 0;
+        while(queue.Count > 0){
+            int position = queue.Dequeue();
+            for(int i = Math.Max(position + minJump, right); i <= Math.Min(n - 1, position + maxJump); i++){
+                if(s[i] == '0' && !visited[i]){
+                    if(i == n - 1) return true;
+                    queue.Enqueue(i);
+                    visited[i] = true;
+                }
+            }
+            right = Math.Min(n - 1, position + maxJump);
+        }
+        return false;
+    }
+}
