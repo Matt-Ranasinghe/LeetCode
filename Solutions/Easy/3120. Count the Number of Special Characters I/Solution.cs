92ff9fd3bd85e071520aad7554dbb9ff8ad8b043
Matt-Ranasinghe <matt.mailathome@gmail.com>
2026-05-26 15:43:02 +0100

    Solution to leetcode problem 3120 in C#.

---
+++ Solutions/Easy/3120. Count the Number of Special Characters I/Solution.cs
@@ -0,0 +1,17 @@
+﻿public class Solution {
+    public int NumberOfSpecialChars(string word) {
+        bool[] upperCase = new bool[26];
+        bool[] lowerCase = new bool[26];
+        foreach(char c in word){
+            if(c < 'Z' + 1){
+                upperCase[c - 'A'] = true;
+            }
+            else lowerCase[c - 'a'] = true;
+        }
+        int result = 0;
+        for(int i = 0; i < 26; i++){
+            result += upperCase[i] && lowerCase[i] ? 1 : 0;
+        }
+        return result;
+    }
+}
