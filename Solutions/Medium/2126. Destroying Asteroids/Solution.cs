e5688b9f807e45d2f428a1215cbdc653eb5063f8
matthew <matt.mailathome@gmail.com>
2026-05-31 20:16:01 +0100

    Solution to leetcode problem 2126 in C#

---
+++ Solutions/Medium/2126. Destroying Asteroids/Solution.cs
@@ -0,0 +1,11 @@
+﻿public class Solution {
+    public bool AsteroidsDestroyed(int mass, int[] asteroids) {
+        Array.Sort(asteroids);
+        long longMass = mass;
+        foreach(int asteroid in asteroids){
+            if(longMass >= asteroid) longMass += asteroid;
+            else return false;
+        }
+        return true;
+    }
+}
