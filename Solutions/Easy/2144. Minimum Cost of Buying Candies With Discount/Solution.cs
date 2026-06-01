1ed00a1f81ab78c23836c1be1768abe7e8312e48
matthew <matt.mailathome@gmail.com>
2026-06-01 16:22:33 +0100

    Solution to leetcode problem 2144 in C#.

---
+++ Solutions/Easy/2144. Minimum Cost of Buying Candies With Discount/Solution.cs
@@ -0,0 +1,11 @@
+﻿public class Solution {
+    public int MinimumCost(int[] cost) {
+        Array.Sort(cost, (a, b) => b.CompareTo(a));
+        int result = 0, n = cost.Length;
+        for(int i = 0; i < n; i += 3){
+            if(i + 1 < n) result += cost[i] + cost[i + 1];
+            else result += cost[i];
+        }
+        return result;
+    }
+}
