656b1633b9a6c9f92b6d5fbbef643fb0c3896e2f
matthew <matt.mailathome@gmail.com>
2026-06-11 19:57:43 +0100

    Solution to leetcode problem 3558 in C#.

---
+++ Solutions/Medium/3558. Number of Ways to Assign Edge Weights I/Solution.cs
@@ -0,0 +1,32 @@
+﻿public class Solution {
+    public int AssignEdgeWeights(int[][] edges) {
+        int MOD = (int)1e9 + 7;
+        Dictionary<int, List<int>> dictEdges = new Dictionary<int, List<int>>();
+        foreach(int[] edge in edges){
+            if(!dictEdges.ContainsKey(edge[0])) dictEdges[edge[0]] = new List<int>();
+            if(!dictEdges.ContainsKey(edge[1])) dictEdges[edge[1]] = new List<int>();
+            dictEdges[edge[0]].Add(edge[1]);
+            dictEdges[edge[1]].Add(edge[0]);
+        }
+        HashSet<int> visited = new HashSet<int>();
+        visited.Add(1);
+        Queue<(int node, int depth)> queue = new Queue<(int node, int depth)>();
+        queue.Enqueue((1, 0));
+        int maxDepth = 1;
+        while(queue.Count > 0){
+            (int node, int depth) nodeData = queue.Dequeue();
+            maxDepth = nodeData.depth;
+            foreach(int neighbour in dictEdges[nodeData.node]){
+                if(!visited.Contains(neighbour)){
+                    visited.Add(neighbour);
+                    queue.Enqueue((neighbour, nodeData.depth + 1));
+                }
+            }
+        }
+        long result = 1;
+        for(int i = 1; i < maxDepth; i++){
+            result = (result * 2) % MOD;
+        }
+        return (int) result;
+    }
+}
