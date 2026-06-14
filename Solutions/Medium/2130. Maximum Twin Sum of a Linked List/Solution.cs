d5279cfcf2154563d9153ea745aed25cff82ce78
matthew <matt.mailathome@gmail.com>
2026-06-14 16:40:34 +0100

    Solution to leetcode problem 2130 in C#.

---
+++ Solutions/Medium/2130. Maximum Twin Sum of a Linked List/Solution.cs
@@ -0,0 +1,26 @@
+﻿
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+         this.next = next;
+    }
+}
+
+public class Solution {
+    public int PairSum(ListNode head) {
+        List<int> listForm = new List<int>();
+        while(head != null){
+            listForm.Add(head.val);
+            head = head.next;
+        }
+        int result = 0;
+        int n = listForm.Count;
+        for(int i = 0; i < n / 2; i++){
+            result = Math.Max(result, listForm[i] + listForm[n - i - 1]);
+        }
+        return result;
+    }
+}
