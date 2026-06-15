a079375acc5dc6a049c683aaa8c0ee97fc4caa50
matthew <matt.mailathome@gmail.com>
2026-06-15 15:12:01 +0100

    Solution to leetcode problem 2095 in C#.

---
+++ Solutions/Medium/2095. Delete the Middle Node of a Linked List/Solution.cs
@@ -0,0 +1,26 @@
+﻿public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    public ListNode DeleteMiddle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = slow.next;
+        return head;
+    }
+}
