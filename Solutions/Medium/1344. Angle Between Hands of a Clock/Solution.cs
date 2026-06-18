9f6538fabc03752a9cf467c939e78b45c6fd33c5
matthew <matt.mailathome@gmail.com>
2026-06-18 12:54:47 +0100

    Solution to leetcode problem 1344 in C#.

---
+++ Solutions/Medium/1344. Angle Between Hands of a Clock/Solution.cs
@@ -0,0 +1,10 @@
+﻿public class Solution {
+    public double AngleClock(int hour, int minutes) {
+        double hourPos = 30 * hour + (double) minutes / 2;
+        double minutePos = 6 * minutes;
+        double smaller = Math.Min(hourPos, minutePos), larger = Math.Max(hourPos, minutePos);
+        double angleOne = larger - smaller;
+        double angleTwo = 360 - larger + smaller;
+        return Math.Min(angleOne, angleTwo);
+    }
+}
