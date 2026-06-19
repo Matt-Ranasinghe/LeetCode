b3734f3be9d51fe613c98e2c73569062ad5c5b99
matthew <matt.mailathome@gmail.com>
2026-06-19 11:18:10 +0100

    Solution to leetcode problem 1732 in C#.

---
+++ Solutions/Easy/1732. Find the Highest Altitude/Solution.cs
@@ -0,0 +1,10 @@
+﻿public class Solution {
+    public int LargestAltitude(int[] gain) {
+        int max = 0, current = 0;
+        foreach(int num in gain){
+            current += num;
+            max = Math.Max(max, current);
+        }
+        return max;
+    }
+}
