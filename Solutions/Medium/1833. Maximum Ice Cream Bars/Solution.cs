f5cf89ea842b90f3807b77bacd85cb4068aefbb0
matthew <matt.mailathome@gmail.com>
2026-06-21 17:17:59 +0100

    Solution to leetcode problem 1833 in C#.

---
+++ Solutions/Medium/1833. Maximum Ice Cream Bars/Solution.cs
@@ -0,0 +1,11 @@
+﻿public class Solution {
+    public int MaxIceCream(int[] costs, int coins) {
+        Array.Sort(costs);
+        int result = 0;
+        for( ; result < costs.Length; result++){
+            if(costs[result] <= coins) coins -= costs[result];
+            else break;
+        }
+        return result;
+    }
+}
