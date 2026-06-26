55f59d1b555dabb5e45f8ecd48bcbf340db48c2d
matthew <matt.mailathome@gmail.com>
2026-06-26 18:48:26 +0100

    Solution to leetcode problem 3739 in C#.

---
+++ Solutions/Hard/3739. Count Subarrays With Majority Element II/Solution.cs
@@ -0,0 +1,22 @@
+﻿public class Solution {
+    public long CountMajoritySubarrays(int[] nums, int target) {
+        int n = nums.Length;
+        int[] pre = new int[n * 2 + 1];
+        pre[n] = 1;
+        int count = n;
+        long ans = 0, presum = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == target) {
+                presum += pre[count];
+                count++;
+                pre[count]++;
+            } else {
+                count--;
+                presum -= pre[count];
+                pre[count]++;
+            }
+            ans += presum;
+        }
+        return ans;
+    }
+}
