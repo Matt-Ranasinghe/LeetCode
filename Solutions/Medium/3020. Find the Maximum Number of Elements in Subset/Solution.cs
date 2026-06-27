26a20af42e9341e4f9a5b13ec095c0a0fea89e8a
matthew <matt.mailathome@gmail.com>
2026-06-28 00:20:56 +0100

    Solution to leetcode problem 3020 in C#.

---
+++ Solutions/Medium/3020. Find the Maximum Number of Elements in Subset/Solution.cs
@@ -0,0 +1,23 @@
+﻿public class Solution {
+    public int MaximumLength(int[] nums) {
+        var freq = new Dictionary<long, int>();
+        foreach (int num in nums) {
+            freq.TryGetValue(num, out int c);
+            freq[num] = c + 1;
+        }
+        freq.TryGetValue(1, out int onefreq);
+        int ans = (onefreq & 1) == 1 ? onefreq : onefreq - 1;
+        freq.Remove(1);
+        foreach (long num in freq.Keys) {
+            int res = 0;
+            long x = num;
+            while (freq.TryGetValue(x, out int c) && c > 1) {
+                res += 2;
+                x *= x;
+            }
+            ans = Math.Max(ans, res + (freq.ContainsKey(x) ? 1 : -1));
+        }
+
+        return ans;
+    }
+}
