f345977e5fb6ab97c5b6b736caf3ef74e1929a84
matthew <matt.mailathome@gmail.com>
2026-06-28 22:28:29 +0100

    Solution to leetcode problem 1846 in C#.

---
+++ Solutions/Medium/1846. Maximum Element After Decreasing and Rearranging/Solution.cs
@@ -0,0 +1,10 @@
+﻿public class Solution {
+    public int MaximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Array.Sort(arr);
+        int result = 0;
+        foreach(int num in arr){
+            if(result < num) result++;
+        }
+        return result;
+    }
+}
