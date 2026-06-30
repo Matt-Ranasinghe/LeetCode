77089b202a6a7ccdbf9159d5777655e7150d8b0f
matthew <matt.mailathome@gmail.com>
2026-06-30 15:24:22 +0100

    Solution to leetcode problem 1358 in C#.

---
+++ Solutions/Medium/1358. Number of Substrings Containing All Three Characters/Solution.cs
@@ -0,0 +1,24 @@
+﻿public class Solution {
+    public int NumberOfSubstrings(string s) {
+        int n = s.Length;
+        int[] chars = new int[3];
+        int leftPointer = 0, rightPointer = 0;
+        int result = 0;
+        foreach(char c in s){
+            rightPointer++;
+            chars[c - 'a']++;
+            bool minChar = true;;
+            foreach(int i in chars){
+                if(i == 0) {
+                    minChar = false;
+                    break;
+                }
+            }
+            while(minChar){
+                result += n - rightPointer + 1;
+                if(--chars[s[leftPointer++] - 'a'] == 0) break;
+            }
+        }
+        return result;
+    }
+}
