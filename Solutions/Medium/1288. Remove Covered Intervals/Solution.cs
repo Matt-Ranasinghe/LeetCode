05d0cfe58fc6bc98e05240603de35904e13cf812
matthew <matt.mailathome@gmail.com>
2026-07-06 16:38:02 +0100

    Solution to leetcode problem 1288 in C#.

---
+++ Solutions/Medium/1288. Remove Covered Intervals/Solution.cs
@@ -0,0 +1,17 @@
+﻿public class Solution {
+    public int RemoveCoveredIntervals(int[][] intervals) {
+        int end = 0, result = 0;
+        Array.Sort(intervals, (a, b) => {
+            int firstComp = a[0].CompareTo(b[0]);
+            if(firstComp != 0) return firstComp;
+            return b[1].CompareTo(a[1]);
+        });
+        foreach(int[] range in intervals){
+            if(end < range[1]){
+                end = range[1];
+                result++;
+            }
+        }
+        return result;
+    }
+}
