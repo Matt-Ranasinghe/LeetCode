3ae80258201f0d2c9e81b374f06bcc6412849050
matthew <matt.mailathome@gmail.com>
2026-07-08 00:14:40 +0100

    Solution to leetcode problem 3754 in C#.

---
+++ Solutions/Easy/3754. Concatenate Non-Zero Digits and Multiply by Sum I/Solution.cs
@@ -0,0 +1,15 @@
+﻿public class Solution {
+    public long SumAndMultiply(int n) {
+        long sum = 0, nonNeg = 0, pos = 1;
+        while(n != 0){
+            int val = n % 10;
+            if(val != 0){
+                nonNeg += pos * val;
+                pos *= 10;
+                sum += val;
+            }
+            n /= 10;
+        }
+        return sum * nonNeg;
+    }
+}
