c68c320396c016d84b43314906a7b60ec988a1c4
matthew <matt.mailathome@gmail.com>
2026-07-09 14:09:22 +0100

    Solution to leetcode problem 3532 in C#.

---
+++ Solutions/Medium/3532. Path Existence Queries in a Graph I/Solution.cs
@@ -0,0 +1,26 @@
+﻿public class Solution {
+    public bool[] PathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        int[] groupings = CreateRange(n, nums, maxDiff);
+        int m = queries.Length;
+        bool[] result = new bool[m];
+        for(int i = 0; i < m; i++){
+            if(groupings[queries[i][0]] == groupings[queries[i][1]]){
+                result[i] = true;
+            }
+        }
+        return result;
+    }
+
+    private int[] CreateRange(int n, int[] nums, int maxDiff){
+        int currentId = 0;
+        int[] result = new int[n];
+        result[0] = currentId;
+        for(int i = 1; i < n; i++){
+            if(nums[i] - nums[i - 1] > maxDiff){
+                currentId++;
+            }
+            result[i] = currentId;
+        }
+        return result;
+    }
+}
