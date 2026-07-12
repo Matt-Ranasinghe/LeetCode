8d427aac2f6eb35997c588143da715856ae25e9e
matthew <matt.mailathome@gmail.com>
2026-07-12 17:08:32 +0100

    Solution to leetcode problem 1331 in C#

---
+++ Solutions/Easy/1331. Rank Transform of an Array/Solution.cs
@@ -0,0 +1,20 @@
+﻿public class Solution {
+    public int[] ArrayRankTransform(int[] arr) {
+        int n = arr.Length;
+        PriorityQueue<(int val, int pos), int> pq = new PriorityQueue<(int val, int pos), int>();
+        for(int i = 0; i < n; i++){
+            pq.Enqueue((arr[i], i), arr[i]);
+        }
+        int current = 0, prev = Int32.MinValue;
+        while(pq.Count > 0){
+            (int val, int pos) dequeued = pq.Dequeue();
+            if(prev < dequeued.val) 
+            {
+                current++;
+                prev = dequeued.val;
+            }
+            arr[dequeued.pos] = current;
+        }
+        return arr;
+    }
+}
