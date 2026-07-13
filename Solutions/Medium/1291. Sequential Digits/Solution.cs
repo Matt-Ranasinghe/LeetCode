930bf8754fd85e04ae42170b9156e53d00b1ae8f
matthew <matt.mailathome@gmail.com>
2026-07-13 12:41:18 +0100

    Solution to leetcode problem 1291 in C#.

---
+++ Solutions/Medium/1291. Sequential Digits/Solution.cs
@@ -0,0 +1,26 @@
+﻿public class Solution {
+    private static readonly List<int> sequentialDigits = new List<int>();
+
+    public IList<int> SequentialDigits(int low, int high) {
+        if(sequentialDigits.Count == 0) buildSequences();
+        IList<int> result = new List<int>();
+        int pos = 0;
+        while(pos < sequentialDigits.Count && sequentialDigits[pos] <= high ){
+            if(sequentialDigits[pos] >= low) result.Add(sequentialDigits[pos]);
+            pos++;
+        }
+        return result;
+    }
+
+    private static void buildSequences(){
+        int current = 0;
+        for(int i = 1; i < 10; i++){
+            current = i;
+            for(int j = i; j < 10; j++){
+                sequentialDigits.Add(current);
+                current = current * 10 + j + 1;
+            }
+        }
+        sequentialDigits.Sort();
+    }
+}
