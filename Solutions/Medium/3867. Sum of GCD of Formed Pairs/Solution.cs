0e06f761cfc439ec0a754c30e0632f476abccd5c
matthew <matt.mailathome@gmail.com>
2026-07-16 22:07:09 +0100

    Solution to leetcode problem 3867 in C#.

---
+++ Solutions/Medium/3867. Sum of GCD of Formed Pairs/Solution.cs
@@ -0,0 +1,31 @@
+﻿public class Solution {
+    public long GcdSum(int[] nums) {
+        int n = nums.Length;
+        int[] max = new int[n];
+        int prefixNum = 0;
+        for(int i = 0; i < n; i++){
+            prefixNum = Math.Max(prefixNum, nums[i]);
+            max[i] = prefixNum;
+        }
+
+        int[] gcds = new int[n];
+        for(int i = 0; i < n; i++){
+            gcds[i] = GCD(nums[i], max[i]);
+        }
+        Array.Sort(gcds);
+        long result = 0;
+        for(int i = 0; i < n/2; i++){
+            result += GCD(gcds[i], gcds[n - i - 1]);
+        }
+        return result;
+    }
+
+    private int GCD(int a, int b){
+        while(a != 0){
+            int temp = a;
+            a = b % a;
+            b = temp;
+        }
+        return b;
+    }
+}
