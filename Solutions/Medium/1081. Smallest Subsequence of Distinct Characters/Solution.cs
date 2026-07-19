0cf56e52a1e390d76a2ad9aa668e05ac6b491103
matthew <matt.mailathome@gmail.com>
2026-07-20 00:57:42 +0100

    Solution to leetcode problem 1081 in C#.

---
+++ Solutions/Medium/1081. Smallest Subsequence of Distinct Characters/Solution.cs
@@ -0,0 +1,24 @@
+﻿public class Solution {
+    public string SmallestSubsequence(string s) {
+        
+        int[] last = new int[26];
+        for (int i = 0; i < s.Length; i++) last[s[i] - 'a'] = i;
+        
+        var stack = new Stack<char>();
+        var seen = new HashSet<char>();
+
+        for (int i = 0; i < s.Length; i++){
+             char c = s[i];
+
+            if (seen.Contains(c))
+                continue;
+
+            while(stack.Count>0 && stack.Peek() > c && last[stack.Peek() -'a'] > i){
+                seen.Remove(stack.Pop());
+            }
+            stack.Push(c);
+            seen.Add(c);
+        }
+        return new string(stack.Reverse().ToArray());
+    }
+}
