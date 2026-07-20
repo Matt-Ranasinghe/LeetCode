72acb91ac55fa6c6e9d7309cba18ea6c5370a675
matthew <matt.mailathome@gmail.com>
2026-07-20 16:33:56 +0100

    Solution to leetcode problem 1260 in C#.

---
+++ Solutions/Easy/1260. Shift 2D Grid/Solution.cs
@@ -0,0 +1,30 @@
+﻿public class Solution {
+    public IList<IList<int>> ShiftGrid(int[][] grid, int k) {
+        var l = grid.Length;
+        var oneGridArray = new List<int>();
+
+        for (int i = 0; i < l; i++)
+        {
+            for (int j = 0; j < grid[i].Length; j++)
+                oneGridArray.Add(grid[i][j]);
+        }
+
+        for (int j = 0; j < k; j++)
+        {
+            int t = oneGridArray[oneGridArray.Count() - 1];
+            for (int i = oneGridArray.Count() - 1; i > 0; i--)
+                oneGridArray[i] = oneGridArray[i - 1];
+            oneGridArray[0] = t;
+        }
+
+        var index = 0;
+        for (int i = 0; i < l; i++)
+            for (int j = 0; j < grid[i].Length; j++)
+            {
+                grid[i][j] = oneGridArray[index];
+                index++;
+            }
+  
+        return grid;
+    }
+}
