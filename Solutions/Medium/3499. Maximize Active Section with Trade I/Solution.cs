47370ad159f0f6a770c00c4a068373d1e42b8399
matthew <matt.mailathome@gmail.com>
2026-07-21 18:25:26 +0100

    Solution to leetcode problem 3499 in C#.

---
+++ Solutions/Medium/3499. Maximize Active Section with Trade I/Solution.cs
@@ -0,0 +1,28 @@
+﻿public class Solution {
+    public int MaxActiveSectionsAfterTrade(string s) {
+        int n = s.Length;
+        int ones = 0;
+        foreach(char c in s){
+            if(c == '1') ones++;
+        }
+        int current = 0;
+        int prev = Int32.MinValue;
+        int bestActivation = 0;
+        char cur = '1';
+        for(int i = 0; i < n; i++){
+            if(cur != s[i]){
+                if(cur == '0') {
+                    bestActivation = Math.Max(prev + current, bestActivation);
+                    prev = current;
+                    current = 0;
+                }
+                cur = s[i];
+            }
+            if(cur == '0'){
+                current++;
+            }
+        }
+        if(cur == '0') bestActivation = Math.Max(prev + current, bestActivation);
+        return ones + bestActivation;
+    }
+}
