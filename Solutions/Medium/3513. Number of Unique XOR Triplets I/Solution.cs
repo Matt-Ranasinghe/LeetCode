ad7b06b9ab995df8f3879fe611d009c3caa3fe17
matthew <matt.mailathome@gmail.com>
2026-07-23 20:06:34 +0100

    Solution to leetcode problem 3513 in C#.

---
+++ Solutions/Medium/3513. Number of Unique XOR Triplets I/Solution.cs
@@ -0,0 +1,8 @@
+﻿public class Solution {
+    public int UniqueXorTriplets(int[] nums) {
+        int n = nums.Length;
+        if(n < 3) return n;
+        int pow = (int)Math.Ceiling(Math.Log(nums.Max() + 1, 2));
+        return (int)Math.Pow(2, pow);
+    }
+}
