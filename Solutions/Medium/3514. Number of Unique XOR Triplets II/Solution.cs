d22e79385570b28c3eda7f6745976c3d06146496
matthew <matt.mailathome@gmail.com>
2026-07-24 12:47:52 +0100

    Solution to leetcode problem 3541 in C#.

---
+++ Solutions/Medium/3514. Number of Unique XOR Triplets II/Solution.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public int UniqueXorTriplets(int[] nums) {
+        int n = nums.Length;
+        HashSet<int> pairs = new HashSet<int>();
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                pairs.Add(nums[i] ^ nums[j]);
+            }
+        }
+        HashSet<int> triples = new HashSet<int>();
+        foreach(int num in pairs){
+            for(int k = 0; k < n; k++){
+                triples.Add(num ^ nums[k]);
+            }
+        }
+        return triples.Count;
+    }
+}
