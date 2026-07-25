09616e97f232cd449230cd0fd51fb1346d4981af
matthew <matt.mailathome@gmail.com>
2026-07-25 14:35:17 +0100

    Solution to leetcode problem 3536

---
+++ Solutions/Easy/3536. Maximum Product of Two Digits/Solution.cs
@@ -0,0 +1,17 @@
+﻿public class Solution {
+    public int MaxProduct(int n) {
+        int largest = 0, second = 0;
+        while(n > 0){
+            int digit = n % 10;
+            if(digit > largest){
+                second = largest;
+                largest = digit;
+            }
+            else if(digit > second){
+                second = digit;
+            }
+            n /= 10;
+        }
+        return second * largest;
+    }
+}
