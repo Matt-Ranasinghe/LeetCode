71ae581a202f23293fdd5abe5c620353482407d2
matthew <matt.mailathome@gmail.com>
2026-07-27 16:05:26 +0100

    Solution to leetcode problem 1464 and 628 in C#.

---
+++ Solutions/Easy/1464. Maximum Product of Two Elements in an Array/Solution.cs
@@ -0,0 +1,15 @@
+﻿public class Solution {
+    public int MaxProduct(int[] nums) {
+        int largest = 0, secondLargest = 0;
+        foreach(int num in nums){
+            if(num > largest){
+                secondLargest = largest;
+                largest = num;
+            }
+            else if(num > secondLargest){
+                secondLargest = num;
+            }
+        }
+        return (largest - 1) * (secondLargest - 1);
+    }
+}
---
+++ Solutions/Easy/628. Maximum Product of Three Numbers/Solution.cs
@@ -0,0 +1,30 @@
+﻿public class Solution {
+    public int MaximumProduct(int[] nums) {
+        int smallest = Int32.MaxValue, secondSmallest = Int32.MaxValue;
+        int largest = Int32.MinValue, secondLargest = Int32.MinValue, thirdLargest = Int32.MinValue;
+        foreach(int num in nums){
+            if(largest < num){
+                thirdLargest = secondLargest;
+                secondLargest = largest;
+                largest = num;
+            }
+            else if(secondLargest < num){
+                thirdLargest = secondLargest;
+                secondLargest = num;
+            }
+            else if(thirdLargest < num){
+                thirdLargest = num;
+            }
+            if(smallest > num){
+                secondSmallest = smallest;
+                smallest = num;
+            }
+            else if(secondSmallest > num){
+                secondSmallest = num;
+            }
+        }
+        int result1 = largest * secondLargest * thirdLargest;
+        int result2 = smallest * secondSmallest * largest;
+        return Math.Max(result1, result2);
+    }
+}
