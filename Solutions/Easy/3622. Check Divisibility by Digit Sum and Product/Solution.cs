cab0b9b55abb2216030302a4a30eca22cdba83f0
matthew <matt.mailathome@gmail.com>
2026-08-22 11:41:59 +0100

    Solution to leetcode problem 3622 in C#.

---
+++ Solutions/Easy/3622. Check Divisibility by Digit Sum and Product/Solution.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public bool CheckDivisibility(int n) {
+        int mult = 1;
+        int sum = 0;
+        int copy = n;
+        while(n != 0){
+            int digit = n % 10;
+            mult *= digit;
+            sum += digit;
+            n /= 10;
+        }
+        return copy % (mult + sum) == 0;
+    }
+}
