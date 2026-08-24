b24314e91021496dce5a1a1c78c0bfa014fc64af
matthew <matt.mailathome@gmail.com>
2026-08-24 20:20:19 +0100

    Solution to leetcode prolem 1872 in C#.

---
+++ Solutions/Hard/1872. Stone Game VIII/Solution.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public int StoneGameVIII(int[] stones) {
+        int n = stones.Length;
+        int[] prefixSum = new int[n];
+        prefixSum[0] = stones[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + stones[i];
+        }
+
+        int[] best = new int[n];
+        best[n - 1] = prefixSum[n - 1];
+        for (int i = n - 2; i >= 1; i--) {
+            best[i] = Math.Max(best[i + 1], prefixSum[i] - best[i + 1]);
+            Console.WriteLine(best[i]);
+        }
+        return best[1];
+    }
+}
