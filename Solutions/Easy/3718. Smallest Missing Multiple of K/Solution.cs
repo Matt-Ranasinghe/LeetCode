16abc2e860a16a69dc91a422ade8b12fa6a2ce9f
matthew <matt.mailathome@gmail.com>
2026-08-25 20:49:07 +0100

    Solution to leetcod problem 3718 in C#.

---
+++ Solutions/Easy/3718. Smallest Missing Multiple of K/Solution.cs
@@ -0,0 +1,11 @@
+﻿public class Solution {
+    public int MissingMultiple(int[] nums, int k) {
+        Array.Sort(nums);
+        int curr = k;
+        foreach(int num in nums){
+            if(num == curr) curr += k;
+            else if(num > curr) return curr;
+        }
+        return curr;
+    }
+}
