3a058c61862219c8f1d56dd14b206a1063ab39ce
matthew <matt.mailathome@gmail.com>
2026-09-03 18:55:49 +0100

    Solution to leetcode problem 3876 in C#.

---
+++ Solutions/Medium/3876. Construct Uniform Parity Array II/Solution.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public bool UniformArray(int[] nums1) {
+        int min = nums1[0];
+        bool hasOdd = false;
+        foreach (int num in nums1) {
+            if (num < min) {
+                min = num;
+            }
+            if ((num & 1) == 1) {
+                hasOdd = true;
+            }
+        }
+        if ((min & 1) == 1) {
+            return true;
+        }
+        return !hasOdd;
+    }
+}
