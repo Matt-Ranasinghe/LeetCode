f72e284c9d4f8e2ed60780672a8a074c8613ba24
matthew <matt.mailathome@gmail.com>
2026-09-09 14:38:56 +0100

    Solution to leetcode problem 3871 in C#.

---
+++ Solutions/Medium/3871. Count Commas in Range II/Solution.cs
@@ -0,0 +1,16 @@
+﻿public class Solution {
+    public long CountCommas(long n) {
+        if(n < 1000) return 0;
+        long maxCommas = 0;
+        long UB = 999999, LB = 999, count = 1;
+        long result = 0;
+        while(UB < n){
+            result += (UB - LB) * count;
+            count++;
+            LB = UB;
+            UB = UB * 1000 + 999;
+        }
+        result += (n - LB) * count;
+        return result;
+    }
+}
