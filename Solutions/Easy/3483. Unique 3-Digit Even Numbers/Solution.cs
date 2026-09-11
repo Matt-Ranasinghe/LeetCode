8a0ee7e7b43c4e35cbf46b070abaf19cfe657b46
matthew <matt.mailathome@gmail.com>
2026-09-11 17:06:08 +0100

    Solution to leetcode problem 3483 in C#.

---
+++ Solutions/Easy/3483. Unique 3-Digit Even Numbers/Solution.cs
@@ -0,0 +1,22 @@
+﻿public class Solution {
+    public int TotalNumbers(int[] digits) {
+        int n = digits.Length;
+        bool[] seen = new bool[1000];
+        int result = 0;
+        for(int i = 0; i < n; i++){
+            if((digits[i] & 1) == 1) continue;
+            for(int j = 0; j < n; j++){
+                if(digits[j] == 0 || j == i) continue;
+                for(int k = 0; k < n; k++){
+                    if(k == i || j == k) continue;
+                    int num = (digits[j] * 100) + (digits[k] * 10) + digits[i];
+                    if(!seen[num]){
+                        result++;
+                        seen[num] = true;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
