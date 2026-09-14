809f2d6f4a9a3880df07420fbc6db70118da6414
matthew <matt.mailathome@gmail.com>
2026-09-14 20:42:41 +0100

    Solution to leetcode problem 851 in C#

---
+++ Solutions/Medium/851. Loud and Rich/Solution.cs
@@ -0,0 +1,28 @@
+﻿public class Solution {
+    public int[] LoudAndRich(int[][] richer, int[] quiet) {
+        int n = quiet.Length;
+        List<int>[] richTable = new List<int>[n];
+        PriorityQueue<(int per, int quiet), int> pq = new PriorityQueue<(int per, int quiet), int>();
+        for(int i = 0; i < n; i++) {
+            richTable[i] = new List<int>();
+            pq.Enqueue((i, quiet[i]), quiet[i]);
+        }
+        foreach(int[] richness in richer){
+            richTable[richness[0]].Add(richness[1]);
+        }
+        int[] result = new int[n];
+        Array.Fill(result, -1);
+        while(pq.Count > 0){
+            (int per, int quiet) dequeued = pq.Dequeue();
+            int person = dequeued.per;
+            int quietness = dequeued.quiet;
+            if(result[person] == -1) result[person] = person;
+            foreach(int other in richTable[person]){
+                if(result[other] != -1) continue;
+                result[other] = result[person];
+                pq.Enqueue((other, quietness), quietness);
+            }
+        }
+        return result;
+    }
+}
