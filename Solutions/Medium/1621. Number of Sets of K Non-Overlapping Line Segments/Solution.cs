ff301a1c012fa414a25d59a860e1513e879e6ee9
matthew <matt.mailathome@gmail.com>
2026-09-16 21:48:08 +0100

    Solution to leetcode problem 1621 in C#.

---
+++ Solutions/Medium/1621. Number of Sets of K Non-Overlapping Line Segments/Solution.cs
@@ -0,0 +1,21 @@
+﻿public class Solution {
+    public int NumberOfSets(int n, int k) {
+        const int MOD = (int) 1e9 + 7;
+        int[] dp = new int[n];
+        int[] sum = new int[n + 1];
+        for (int j = 0; j < n; j++) {
+            dp[j] = 1;
+            sum[j + 1] = (sum[j] + dp[j]) % MOD;
+        }
+        for (int i = 1; i <= k; i++) {
+            dp[0] = 0;
+            for (int j = 1; j < n; j++){
+                dp[j] = (dp[j - 1] + sum[j]) % MOD;
+            }
+            for (int j = 0; j < n; j++){
+                sum[j + 1] = (sum[j] + dp[j]) % MOD;
+            }
+        }
+        return dp[n - 1];
+    }
+}
