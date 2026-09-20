af5905541bf478c04225bc609ed774e3d34a892a
matthew <matt.mailathome@gmail.com>
2026-09-20 19:34:08 +0100

    Solution to leetcode problem 3498 in C#.

---
+++ Solutions/Easy/3498. Reverse Degree of a String/Solution.cs
@@ -0,0 +1,9 @@
+﻿public class Solution {
+    public int ReverseDegree(string s) {
+        int result = 0, n = s.Length;
+        for(int i = 0; i < n; i++){
+            result += ('z' - s[i] + 1) * (i + 1);
+        }
+        return result;
+    }
+}
