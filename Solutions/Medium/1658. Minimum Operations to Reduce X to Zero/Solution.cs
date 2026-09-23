805dae5ca95cf9857147c9b1e36a8a9666dc3d66
matthew <matt.mailathome@gmail.com>
2026-09-23 16:15:40 +0100

    Solution to leetcode problem 1658 in C#.

---
+++ Solutions/Medium/1658. Minimum Operations to Reduce X to Zero/Solution.cs
@@ -0,0 +1,26 @@
+﻿public class Solution {
+    public int MinOperations(int[] nums, int x) {
+        int n = nums.Length;
+        int left = 0, right = n - 1;
+        int total = 0, result = Int32.MaxValue;
+        while(left < n && total + nums[left] <= x){
+            total += nums[left];
+            left++;
+        }
+        if(total == x) {
+            result = left;
+            if(left == n) return n;
+        }
+        else if(left == n) return -1;
+        while(total + nums[right] <= x || left > 0){
+            while(total + nums[right] > x && left > 0){
+                left--;
+                total -= nums[left];
+            }
+            total += nums[right];
+            if(total == x) result = Math.Min(result, n - right + left);
+            right--;
+        }
+        return result == Int32.MaxValue ? -1 : result;
+    }
+}
