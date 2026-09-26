867c8e322b43eab4feaef338e70cf8b61333e186
matthew <matt.mailathome@gmail.com>
2026-09-26 16:36:33 +0100

    Solution to leetcode problem 1807 in C#.

---
+++ Solutions/Medium/1807. Evaluate the Bracket Pairs of a String/Solution.cs
@@ -0,0 +1,26 @@
+﻿public class Solution {
+    public string Evaluate(string s, IList<IList<string>> knowledge) {
+        int n = s.Length;
+        Dictionary<string, string> knowledgeDict = new Dictionary<string, string>();
+        foreach(List<string> pair in knowledge){
+            knowledgeDict[pair[0]] = pair[1];
+        }
+        string result = "";
+        for(int i = 0; i < n; i++){
+            if(s[i] == '('){
+                string key = "";
+                i++;
+                while(s[i] != ')'){
+                    key = key + s[i];
+                    i++;
+                }
+                if(knowledgeDict.ContainsKey(key)){
+                    result = result + knowledgeDict[key];
+                }
+                else result = result + '?';
+            }
+            else result = result + s[i];
+        }
+        return result;
+    }
+}
